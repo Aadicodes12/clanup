addb80c7da471f1b00cebf86cb6fe33b96470455
[dyad] <aadityawaykole@gmail.com>
2026-03-11 11:27:18 +0530

    Reverted all changes back to version 7925cab19bf5dc7b5d90d1136fb2e83b6878e654

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import AnimatedText from '@/components/AnimatedText';
-import PhoneMockup from '@/components/PhoneMockup'; // Import the new component
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-black text-white flex flex-col">
+    <div className="min-h-screen bg-black text-white">
       {/* Navbar */}
       <nav className="bg-neutral-900 p-4 flex justify-between items-center">
         <div className="text-lg font-bold font-sans">Clanup</div>
@@ -14,8 +13,8 @@ const Index = () => {
       <div className="border-b-2 border-white w-full"></div>
 
       {/* Main content area */}
-      <div className="flex flex-col items-center flex-grow p-4 pt-12 space-y-12"> {/* Removed justify-center, added pt-12 and space-y-12 */}
-        <div className="text-center"> {/* Removed mt-16 and mb-12, let space-y-12 handle spacing */}
+      <div className="flex flex-col items-center justify-center flex-grow p-4">
+        <div className="mt-16 text-center">
           <AnimatedText className="text-4xl font-bold font-sans">
             Great hacks start with great teams
           </AnimatedText>
@@ -23,14 +22,6 @@ const Index = () => {
             Build your hackathon team in minutes
           </p>
         </div>
-        
-        {/* Phone Mockup */}
-        <PhoneMockup>
-          {/* You can add content inside the phone screen here */}
-          <div className="flex items-center justify-center h-full text-gray-400 text-sm">
-            Your app content goes here!
-          </div>
-        </PhoneMockup>
       </div>
     </div>
   );
