ba14616176149d777022b49f5137ddc7a61fe422
[dyad] <aadityawaykole@gmail.com>
2026-03-11 14:16:57 +0530

    [dyad] Changed the swirling orb's interior to be all white with a subtle grey swirl - wrote 1 file(s)

--- src/components/SwirlingOrb.tsx
+++ src/components/SwirlingOrb.tsx
@@ -9,15 +9,14 @@ const SwirlingOrb: React.FC<SwirlingOrbProps> = ({ className }) => {
   return (
     <div
       className={cn(
-        "relative w-60 h-60 rounded-full flex items-center justify-center overflow-hidden bg-black", // Main container, black background
+        "relative w-60 h-60 rounded-full flex items-center justify-center overflow-hidden bg-white", // Changed to bg-white
         "before:content-[''] before:absolute before:rounded-full",
-        "before:bg-[conic-gradient(from_0deg_at_50%_50%,_rgba(255,255,255,0.8)_0%,_rgba(255,255,255,0)_50%,_rgba(255,255,255,0.8)_100%)]", // Pure white to transparent swirl
+        "before:bg-[conic-gradient(from_0deg_at_50%_50%,_rgba(200,200,200,0.8)_0%,_rgba(200,200,200,0)_50%,_rgba(200,200,200,0.8)_100%)]", // Subtle grey swirl
         "before:w-[150%] before:h-[150%] before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:animate-orb-swirl", // Centered positioning
         className
       )}
     >
-      {/* Inner element for the white mist effect */}
-      <div className="absolute inset-0 rounded-full bg-radial-gradient-mist opacity-70"></div>
+      {/* The inner mist element has been removed as the orb's background is now white */}
     </div>
   );
 };
