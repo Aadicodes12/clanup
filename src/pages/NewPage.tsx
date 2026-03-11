5067cbb1a395d4a0ef2f735d4a8e774d631a5345
[dyad] <aadityawaykole@gmail.com>
2026-03-11 14:23:10 +0530

    [dyad] Removed text from the new page - wrote 1 file(s)

--- src/pages/NewPage.tsx
+++ src/pages/NewPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NewPage = () => {
   return (
     <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Welcome to the New Page!</h1>
+      {/* Text removed */}
     </div>
   );
 };
