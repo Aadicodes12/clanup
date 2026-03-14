b57b0df82849a357baa3b68e6bbff6e65ed1cdba
[dyad] <aadityawaykole@gmail.com>
2026-03-14 17:14:47 +0530

    [dyad] Changed button font to Roboto - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -40,13 +40,13 @@ const Index = () => {
           />
           <div className="flex flex-col w-full items-center">
             <Button 
-              className="mt-8 bg-[#7be382] hover:bg-[#6ad071] text-black font-bold py-3 px-6 text-lg rounded-full transition-all duration-300 transform hover:scale-105 font-arial w-64 max-w-full"
+              className="mt-8 bg-[#7be382] hover:bg-[#6ad071] text-black font-bold py-3 px-6 text-lg rounded-full transition-all duration-300 transform hover:scale-105 font-roboto w-64 max-w-full"
               onClick={() => console.log('Browse Teams clicked')}
             >
               Browse Teams
             </Button>
             <Button 
-              className="mt-4 bg-black border border-gray-400 text-white font-bold py-3 px-6 text-lg rounded-full transition-all duration-300 transform hover:scale-105 font-arial w-64 max-w-full"
+              className="mt-4 bg-black border border-gray-400 text-white font-bold py-3 px-6 text-lg rounded-full transition-all duration-300 transform hover:scale-105 font-roboto w-64 max-w-full"
               onClick={() => console.log('Sign Up clicked')}
             >
               Sign Up
