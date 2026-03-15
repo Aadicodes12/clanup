bd5535648006f8bc3f996ee7a62c0a27b07b17f3
[dyad] <aadityawaykole@gmail.com>
2026-03-15 11:37:42 +0530

    [dyad] wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,11 +6,11 @@ import TrustedByLogos from '../components/TrustedByLogos';
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-start p-4 bg-gray-900">
-      {/* Your header or other components can go here */}
+      {/* Welcome header */}
       <h1 className="text-3xl font-bold text-white mb-8">Welcome to the Hackathon App</h1>
-      {/* Trusted by logos below header */}
+      {/* Trusted by logos component */}
       <TrustedByLogos />
-      {/* Rest of the main page content */}
+      {/* Add any other main page content here */}
     </div>
   );
 };
